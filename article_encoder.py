268bfa7681652660cc8df3d927e2768f5c6974b2
Jered <dmciner1@jhu.edu>
2017-05-08 22:03:30 -0400

    concatenated articles and stats

---
+++ article_encoder.py
@@ -0,0 +1,11 @@
+import os.path
+
+print("hello world")
+
+from keras.layers.embeddings import Embedding
+
+
+from keras.layers import Input, Dense
+from keras.models import Model
+print("keras successfully imported!")
+
